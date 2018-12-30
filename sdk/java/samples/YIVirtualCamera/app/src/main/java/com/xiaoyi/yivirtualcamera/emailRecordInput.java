a73b6bfb29e58217a1a40eafd7dcc62400adbcf4
Jason Ferens <githubcollective@gmail.com>
2018-12-30 13:52:47 -0500

    RC-6 finished most of the email recordscreen elements-integrating preview camera next

--- sdk/java/samples/YIVirtualCamera/app/src/main/java/com/xiaoyi/yivirtualcamera/emailRecordInput.java
+++ sdk/java/samples/YIVirtualCamera/app/src/main/java/com/xiaoyi/yivirtualcamera/emailRecordInput.java
@@ -3,6 +3,8 @@ package com.xiaoyi.yivirtualcamera;
 import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 public class emailRecordInput extends AppCompatActivity {
@@ -22,5 +24,25 @@ public class emailRecordInput extends AppCompatActivity {
         step2.setTypeface(hoopDemoBold);
         step3.setTypeface(hoopDemoBold);
         rollcam.setTypeface(hoopDemoBold);
+
+        //get the spinner from the xml.
+        Spinner dropdown1 = (Spinner) findViewById(R.id.spinner1);
+        //create a list of items for the spinner.
+        String[] items1 = new String[]{"5 seconds", "10 seconds", "30 seconds"};
+        //create an adapter to describe how the items are displayed, adapters are used in several places in android.
+        //There are multiple variations of this, but this is the basic variant.
+        ArrayAdapter<String> adapter1 = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, items1);
+        //set the spinners adapter to the previously created one.
+        dropdown1.setAdapter(adapter1);
+
+        //get the spinner from the xml.
+        Spinner dropdown2 = (Spinner) findViewById(R.id.spinner2);
+        //create a list of items for the spinner.
+        String[] items2 = new String[]{"30 seconds", "1 minute", "5 minutes"};
+        //create an adapter to describe how the items are displayed, adapters are used in several places in android.
+        //There are multiple variations of this, but this is the basic variant.
+        ArrayAdapter<String> adapter2 = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, items2);
+        //set the spinners adapter to the previously created one.
+        dropdown2.setAdapter(adapter2);
     }
 }
